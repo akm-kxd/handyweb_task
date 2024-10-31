22ccd8d4501f9f61f6a8ac31c84a226dc0e5393c
Akmal <akm.kxd@gmail.com>
2024-10-31 20:30:43 +0500

    feat: add redux persist for favorites

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -2,11 +2,18 @@
 
 import { FC, PropsWithChildren } from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import { store } from '@/store';
+import { store, persistor } from '@/store';
 
 const StoreProvider: FC<PropsWithChildren> = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default StoreProvider;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux';
 
 import favoritesReducer from './features/favoritesSlice';
 
-export const store = configureStore({
-  reducer: {
-    favorites: favoritesReducer,
-  },
-  devTools: process.env.NODE_ENV !== 'production',
+// Конфигурация persist
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  whitelist: ['favorites'],
+};
+
+const rootReducer = combineReducers({
+  favorites: favoritesReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
